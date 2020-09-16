6cb74c60b52aa6a48225b2ea38e34fd0aa14eea7
unknown <joesutliff1016@gmail.com>
2020-09-15 20:38:55 -0700

    fourth commit

--- js/my_js.js
+++ js/my_js.js
@@ -20,22 +20,3 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
 }
 
-function initMap() {
-  var options = {
-    zoom: 5,
-    center:{lat:47.6062,lng:-122.3321}
-  }
-
-  var map = new
-  google.maps.Map(document.getElementById('map'), options);
-}
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginRight = "250px";
-}
-
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginRight = "0";
-}
